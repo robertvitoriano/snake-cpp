6f8fb72c2d3f6b07e361a22907958b2b818bac2f
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-11-15 18:19:59 -0300

    remove snake space between parts

--- GameConstants.h
+++ GameConstants.h
@@ -6,6 +6,6 @@ const int WINDOW_HEIGHT = 600;
 const int BASIC_UNITY_SIZE = 32;
 const int SNAKE_SPEED = 10;
 const int FOOD_SPEED = 5;
-const int SPACE_BETWEEN_BODY_PARTS = 24;
+const int SPACE_BETWEEN_BODY_PARTS = 0;
 
 #endif
