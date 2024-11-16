c871ab4dfe841b10aefce9e5c92ef1b9c2ca454a
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-11-16 18:44:12 -0300

    fix snake class

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,8 +1,8 @@
 #include "Snake.h"
+
 #include "GameConstants.h"
 
 Snake::Snake(int xPos, int yPos) {
-
   SDL_Rect head = {xPos, yPos, BASIC_UNITY_SIZE, BASIC_UNITY_SIZE};
 
   direction = RIGHT;
@@ -16,42 +16,39 @@ void Snake::render(SDL_Renderer *renderer, SDL_Texture *spritesheetTexture) {
   }
 
   SDL_Rect snakeHeadSrcRect = {32, 32, BASIC_UNITY_SIZE, BASIC_UNITY_SIZE};
-  SDL_Rect bodySrcRect = {32, 0, BASIC_UNITY_SIZE, BASIC_UNITY_SIZE};
 
   double angle = 0.0;
 
   switch (direction) {
-  case RIGHT:
-    angle = 90.0;
-    break;
-  case DOWN:
-    angle = 180.0;
-    break;
-  case LEFT:
-    angle = -90;
-    break;
-  case UP:
-    angle = 0;
-    break;
+    case RIGHT:
+      angle = 90.0;
+      break;
+    case DOWN:
+      angle = 180.0;
+      break;
+    case LEFT:
+      angle = -90;
+      break;
+    case UP:
+      angle = 0;
+      break;
   }
 
-  SDL_RenderCopyEx(renderer, spritesheetTexture, &snakeHeadSrcRect, &body[0],
-                   angle, nullptr, SDL_FLIP_NONE);
+  SDL_RenderCopyEx(renderer, spritesheetTexture, &snakeHeadSrcRect, &body[0], angle, nullptr, SDL_FLIP_NONE);
 
   for (size_t i = 1; i < body.size(); ++i) {
+    SDL_Rect bodySrcRect = {32, 0, BASIC_UNITY_SIZE, BASIC_UNITY_SIZE};
     SDL_RenderCopy(renderer, spritesheetTexture, &bodySrcRect, &body[i]);
   }
 }
 
 void Snake::moveY(int dy) {
-
   if (dy > 0)
     direction = DOWN;
   else if (dy < 0)
     direction = UP;
 
   for (size_t index = 0; index < body.size(); ++index) {
-
     SDL_Rect &segment = body[index];
     if (segment.y < 0) {
       segment.y = 0;
@@ -66,14 +63,12 @@ void Snake::moveY(int dy) {
   }
 }
 void Snake::moveX(int dx) {
-
   if (dx > 0)
     direction = RIGHT;
   else if (dx < 0)
     direction = LEFT;
 
   for (size_t index = 0; index < body.size(); ++index) {
-
     SDL_Rect &segment = body[index];
 
     if (index == 0) {
@@ -88,12 +83,10 @@ void Snake::moveX(int dx) {
       segment.x = WINDOW_WIDTH - BASIC_UNITY_SIZE;
     }
     if (dx > 0) {
-      segment.x =
-          body[index - 1].x - BASIC_UNITY_SIZE - SPACE_BETWEEN_BODY_PARTS;
+      segment.x = body[index - 1].x - BASIC_UNITY_SIZE - SPACE_BETWEEN_BODY_PARTS;
     }
     if (dx < 0) {
-      segment.x =
-          body[index - 1].x + BASIC_UNITY_SIZE + SPACE_BETWEEN_BODY_PARTS;
+      segment.x = body[index - 1].x + BASIC_UNITY_SIZE + SPACE_BETWEEN_BODY_PARTS;
     }
 
     segment.x += dx;
@@ -103,14 +96,12 @@ const std::vector<SDL_Rect> &Snake::getBody() const { return body; }
 
 void Snake::increaseSize() {
   if (direction == RIGHT) {
-    body.push_back(
-        {(int)body[body.size() - 1].x - BASIC_UNITY_SIZE * (int)body.size(),
-         (int)body[0].y, BASIC_UNITY_SIZE, BASIC_UNITY_SIZE});
+    body.push_back({(int)body[body.size() - 1].x - BASIC_UNITY_SIZE * (int)body.size(), (int)body[0].y,
+                    BASIC_UNITY_SIZE, BASIC_UNITY_SIZE});
   }
   if (direction == LEFT) {
-    body.push_back(
-        {(int)body[body.size() - 1].x + BASIC_UNITY_SIZE * (int)body.size(),
-         (int)body[0].y, BASIC_UNITY_SIZE, BASIC_UNITY_SIZE});
+    body.push_back({(int)body[body.size() - 1].x + BASIC_UNITY_SIZE * (int)body.size(), (int)body[0].y,
+                    BASIC_UNITY_SIZE, BASIC_UNITY_SIZE});
   }
 }
 
